9e090e2f64b3793df2a766fe42f8457727b3020f
hirrao <git@hirrao.com>
2025-07-11 16:49:18 +0800

    init commit server

---
+++ server/build.gradle.kts
@@ -0,0 +1,48 @@
+plugins {
+    java
+    alias(libs.plugins.spring.boot)
+    alias(libs.plugins.dependency.management)
+}
+
+group = "com.hirrao"
+version = "0.0.1-SNAPSHOT"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+repositories {
+    mavenLocal()
+    mavenCentral()
+}
+
+dependencies {
+    implementation(libs.spring.boot.starter.web)
+    implementation(libs.spring.boot.starter.data.redis)
+    implementation(libs.spring.boot.starter.security)
+    implementation(libs.jjwt.api)
+    implementation(libs.druid.spring.boot.starter)
+    implementation(libs.mybatis.plus.starter)
+    implementation(libs.mybatis.plus.jsqlparser)
+    implementation(libs.mapstruct)
+
+    testImplementation(libs.mybatis.plus.starter.test)
+    testImplementation(libs.spring.boot.starter.test)
+
+    compileOnly(libs.lombok)
+
+    runtimeOnly(libs.mysql.connector)
+
+    annotationProcessor(libs.lombok)
+    annotationProcessor(libs.mapstruct.processor)
+}
+
+tasks.withType<JavaCompile> {
+    options.encoding = "UTF-8"
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
